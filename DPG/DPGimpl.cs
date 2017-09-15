eabd1d51bee1104780272e8344d62b30b4cbe8a5
Lukas Reuter <lukas@bootsmann-games.de>
2017-09-15 11:53:33 +0200

    added first android and ios version

---
+++ DPG/DPGimpl.cs
@@ -0,0 +1,186 @@
+/**
+ * Ported from dpg.java by Lukas Reuter
+ * Copyright Lukas Reuter 2017
+ **/
+
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Xamarin.Forms;
+
+public class DPGimpl
+{
+    bool debug;
+    bool phrase;
+    bool big = true;
+
+    string human = "";
+    string result = "";
+
+    int clrPos;
+
+    const int ITERATIONS = 32768;
+    const int KEY_LENGTH_IN_BYTES = 64;
+
+    readonly char[] hex_array = { '0', '1', '2', '3', '4', '5', '6', '7',
+                                  '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+    // This is a FIFO data structure similar to std::queue in C++
+    readonly Queue<int> _numberQueue = new Queue<int>();
+    StringBuilder _readableSB = new StringBuilder();
+
+    /**
+    * Method to make passwords easy for humans to read
+    **/
+    string MakeReadablePassword(string password)
+    {
+        int x = 0;
+        _readableSB.Clear();
+        while (x < password.Length)
+        {
+            _readableSB.Append(password.Substring(x, 3));
+            _readableSB.Append(" ");
+            x += 3;
+        }
+        return _readableSB.ToString();
+    }
+
+    /**
+    * A method to get 64 random bytes deterministically based on user input
+    * And to also fill the queue with numbers
+    **/
+    void FillBytes(string sentence, string word)
+    {
+        byte[] salt = Encoding.UTF8.GetBytes(word);
+        string password = sentence;
+        // 512 bit key_length (64 bytes). Python and C++ use bytes rather than bits.
+        //TODO: replace with custom impl
+        byte[] random_bytes = KeyDerivation.Pbkdf2(password, salt,
+                                             KeyDerivationPrf.HMACSHA512,
+                                             ITERATIONS, KEY_LENGTH_IN_BYTES);
+
+        foreach (var b in random_bytes)
+        {
+            // & 0xFF ensures the int is unsigned
+            _numberQueue.Enqueue(b & 0xFF);
+        }
+    }
+
+    // NOTE: The iteration count should
+    // be as high as possible without causing
+    // unreasonable delay.  Note also that the password
+    // and salt are byte arrays, not strings.  After use,
+    // the password and salt should be cleared (with Array.Clear)
+    public static byte[] PBKDF2Sha256GetBytes(int dklen, byte[] password, byte[] salt, int iterationCount)
+    {
+        using (var hmac = new System.Security.Cryptography.HMACSHA256(password))
+        {
+            int hashLength = hmac.HashSize / 8;
+            if ((hmac.HashSize & 7) != 0)
+                hashLength++;
+            int keyLength = dklen / hashLength;
+            if ((long)dklen > (0xFFFFFFFFL * hashLength) || dklen < 0)
+                throw new ArgumentOutOfRangeException("dklen");
+            if (dklen % hashLength != 0)
+                keyLength++;
+            byte[] extendedkey = new byte[salt.Length + 4];
+            Buffer.BlockCopy(salt, 0, extendedkey, 0, salt.Length);
+            using (var ms = new System.IO.MemoryStream())
+            {
+                for (int i = 0; i < keyLength; i++)
+                {
+                    extendedkey[salt.Length] = (byte)(((i + 1) >> 24) & 0xFF);
+                    extendedkey[salt.Length + 1] = (byte)(((i + 1) >> 16) & 0xFF);
+                    extendedkey[salt.Length + 2] = (byte)(((i + 1) >> 8) & 0xFF);
+                    extendedkey[salt.Length + 3] = (byte)(((i + 1)) & 0xFF);
+                    byte[] u = hmac.ComputeHash(extendedkey);
+                    Array.Clear(extendedkey, salt.Length, 4);
+                    byte[] f = u;
+                    for (int j = 1; j < iterationCount; j++)
+                    {
+                        u = hmac.ComputeHash(u);
+                        for (int k = 0; k < f.Length; k++)
+                        {
+                            f[k] ^= u[k];
+                        }
+                    }
+                    ms.Write(f, 0, f.Length);
+                    Array.Clear(u, 0, u.Length);
+                    Array.Clear(f, 0, f.Length);
+                }
+                byte[] dk = new byte[dklen];
+                ms.Position = 0;
+                ms.Read(dk, 0, dklen);
+                ms.Position = 0;
+                for (long i = 0; i < ms.Length; i++)
+                {
+                    ms.WriteByte(0);
+                }
+                Array.Clear(extendedkey, 0, extendedkey.Length);
+                return dk;
+            }
+        }
+    }
+
+    /**
+     * The GUI
+     */
+    public DPGimpl()
+    {
+        var sentence = "";//txtSentence.getPassword();
+        var word = "";//txtWord.getPassword();
+        result = "";
+        _numberQueue.Clear();
+
+        //TODO: change these
+        if (sentence.Length == 0)
+        {
+            Device.BeginInvokeOnMainThread(async () => 
+                    await Application.Current.MainPage.DisplayAlert("Ups :(", "The sentence is empty.", "OK"));
+            return;
+        }
+        if (word.Length == 0)
+        {
+			Device.BeginInvokeOnMainThread(async () =>
+		            await Application.Current.MainPage.DisplayAlert("Ups :(", "The word is empty.", "OK"));
+            return;
+        }
+
+        //try
+        //{
+            FillBytes(sentence, word);
+
+            if (phrase)
+            {
+                var words = Jars.Words();
+                for (var i = 0; i < 7; ++i)
+                {
+                    result += words[_numberQueue.Dequeue()];
+                }
+                result += Jars.Special()[_numberQueue.Dequeue()];
+                result += Jars.Upper()[_numberQueue.Dequeue()];
+                result += Jars.Numbers()[_numberQueue.Dequeue()];
+            }
+            else
+            {
+                var iter = big ? (24 - 3) : (12 - 3);
+
+                var lower = Jars.Lower();
+                while (result.Length < iter)
+                {
+                    result += lower[_numberQueue.Dequeue()];
+                }
+                result += Jars.Special()[_numberQueue.Dequeue()];
+                result += Jars.Upper()[_numberQueue.Dequeue()];
+                result += Jars.Numbers()[_numberQueue.Dequeue()];
+            }
+        //}
+        /*catch (NoSuchAlgorithmException exc)
+        {
+        }
+        catch (InvalidKeySpecException exc)
+        {
+        }*/
+
+        //TODO: copy to clipboard here
+    }
+}
---
+++ DPG/Jars.cs
@@ -0,0 +1,134 @@
+using System.Collections.Generic;
+
+public static class Jars
+{
+    public static Dictionary<int, string> Numbers()
+    {
+        var mp = new Dictionary<int, string>();
+
+        while (mp.Count < 256)
+        {
+            mp.Add(mp.Count, "2");
+            mp.Add(mp.Count, "3");
+            mp.Add(mp.Count, "4");
+            mp.Add(mp.Count, "5");
+            mp.Add(mp.Count, "6");
+            mp.Add(mp.Count, "7");
+            mp.Add(mp.Count, "8");
+            mp.Add(mp.Count, "9");
+        }
+
+        return mp;
+    }
+
+    public static Dictionary<int, string> Special()
+    {
+        var mp = new Dictionary<int, string>();
+
+        while (mp.Count < 256)
+        {
+            mp.Add(mp.Count, ".");
+            mp.Add(mp.Count, ",");
+            mp.Add(mp.Count, "!");
+            mp.Add(mp.Count, "*");
+            mp.Add(mp.Count, "#");
+            mp.Add(mp.Count, "&");
+            mp.Add(mp.Count, "^");
+            mp.Add(mp.Count, "~");
+        }
+
+        return mp;
+    }
+
+    public static Dictionary<int, string> Lower()
+    {
+        Dictionary<int, string> mp = new Dictionary<int, string>();
+
+        while (mp.Count < 256)
+        {
+            mp.Add(mp.Count, "a");
+            mp.Add(mp.Count, "b");
+            mp.Add(mp.Count, "c");
+            mp.Add(mp.Count, "d");
+            mp.Add(mp.Count, "e");
+            mp.Add(mp.Count, "f");
+            mp.Add(mp.Count, "g");
+            mp.Add(mp.Count, "h");
+            mp.Add(mp.Count, "j");
+            mp.Add(mp.Count, "k");
+            mp.Add(mp.Count, "m");
+            mp.Add(mp.Count, "n");
+            mp.Add(mp.Count, "p");
+            mp.Add(mp.Count, "r");
+            mp.Add(mp.Count, "s");
+            mp.Add(mp.Count, "t");
+        }
+
+        return mp;
+    }
+
+    public static Dictionary<int, string> Upper()
+    {
+        Dictionary<int, string> mp = new Dictionary<int, string>();
+
+        while (mp.Count < 256)
+        {
+            mp.Add(mp.Count, "A");
+            mp.Add(mp.Count, "B");
+            mp.Add(mp.Count, "C");
+            mp.Add(mp.Count, "D");
+            mp.Add(mp.Count, "E");
+            mp.Add(mp.Count, "F");
+            mp.Add(mp.Count, "G");
+            mp.Add(mp.Count, "H");
+            mp.Add(mp.Count, "J");
+            mp.Add(mp.Count, "K");
+            mp.Add(mp.Count, "M");
+            mp.Add(mp.Count, "N");
+            mp.Add(mp.Count, "P");
+            mp.Add(mp.Count, "R");
+            mp.Add(mp.Count, "S");
+            mp.Add(mp.Count, "T");
+        }
+
+        return mp;
+    }
+
+    public static Dictionary<int, string> Words()
+    {
+        var mp = new Dictionary<int, string>();
+
+        var word_list = new string[]
+        {
+            "and", "ask", "ass", "ape", "ate", "axe", "air", "aim", "ana", "awe", "act", "add", "age", "all", "ant",
+            "bat", "ban", "bar", "bed", "bee", "bet", "bit", "bug", "bob", "bot", "boy", "bud", "but",
+            "cab", "can", "cap", "cat", "car", "cog", "con", "cop", "cot", "cow", "coy", "cub", "cut",
+            "dad", "dam", "dan", "day", "den", "did", "dig", "dip", "doc", "dog", "don", "dot", "dry", "dug",
+            "ear", "eat", "egg", "ego", "elf", "elk", "elm", "end", "eye", "eve",
+            "fad", "fan", "far", "fat", "fax", "fig", "fit", "fix", "fly", "few", "foe", "fog", "for", "fur",
+            "gag", "gap", "gel", "gem", "get", "god", "goo", "got", "gum", "gun", "gut", "guy", "gym",
+            "hot", "how", "has", "had", "ham", "hat", "him", "her", "hit", "hop",
+            "ice", "icy", "ill", "ink", "inn", "ion", "its", "ivy",
+            "jam", "jar", "jaw", "jay", "jet", "jim", "joe", "jog", "jot", "joy", "jug",
+            "keg", "ken", "key", "kid", "kim", "kit", "kin",
+            "lab", "lad", "lap", "law", "lie", "lee", "let", "lip", "lob", "log", "lot", "low", "lug",
+            "mac", "mag", "map", "man", "mat", "max", "meg", "men", "met", "mom", "moo", "mop", "mow", "mud", "mug", "mut",
+            "nab", "nag", "nap", "net", "new", "nip", "nod", "not", "now", "nun", "nut",
+            "oak", "oat", "oar", "off", "oil", "old", "one", "our", "out", "own",
+            "pan", "pal", "pam", "pat", "pea", "pen", "pet", "pig", "pit", "pot",
+            "rag", "ray", "run", "ram", "ran", "rap", "rat", "rig", "rip", "rob", "ron", "rot",
+            "sad", "sag", "sam", "sat", "say", "see", "sex", "set", "she", "shy", "sin", "sir", "sit", "sky", "soy", "sun",
+            "tan", "tap", "tar", "tea", "ted", "too", "the", "tim", "tip", "toe", "tom", "toy",
+            "wag", "was", "wax", "way", "web", "wee", "wet", "why", "wig", "win", "wow", "won",
+            "yak", "yam", "yap", "yen", "yep", "yes", "yet", "yew", "you", "yum",
+            "zag", "zig", "zit", "zap", "zip", "zoo"
+        };
+
+        foreach (var word in word_list)
+        {
+            mp.Add(mp.Count, word);
+        }
+
+        return mp;
+    }
+}
